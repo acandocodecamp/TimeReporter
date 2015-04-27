0511db7b912167d83b1fad19fd2001170b66659d
hemant.bam@gmail.com <hmt@dev12>
2015-04-27 20:52:21 +0200

    service added in sync worker

---
+++ source/Acando.TimeReporter/Acando.TimeReporter.ViewModelSyncWorker/Service/TimereportService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Acando.TimeReporter.ViewModelSyncWorker.Contracts;
+using Acando.TimeReporter.ViewModelSyncWorker.Repository;
+
+namespace Acando.TimeReporter.ViewModelSyncWorker.Service
+{
+    public class TimereportService
+    {
+        private readonly ITimeReportWorkerrepository _repository;
+
+        public TimereportService()
+        {
+           _repository= new TimeReportWorkerrepository();
+        }
+
+        public void UpdateTimeReport(ITimeReportViewModel model)
+        {
+            _repository.UpdateTimeReport(model);
+        }
+
+    }
+}
