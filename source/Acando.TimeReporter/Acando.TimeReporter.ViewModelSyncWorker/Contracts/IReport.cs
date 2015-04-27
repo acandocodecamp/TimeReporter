0b0ea35ba0e40e88fe64e7aab623f9442e58cd51
hemant.bam@gmail.com <hmt@dev12>
2015-04-27 18:44:59 +0200

    added time reporter worker role

---
+++ source/Acando.TimeReporter/Acando.TimeReporter.ViewModelSyncWorker/Contracts/IReport.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acando.TimeReporter.ViewModelSyncWorker.Contracts
+{
+    public interface IReport
+    {
+        string ProjectNumber { get; set; }
+        int Hours { get; set; }
+        string DisplayName { get; set; }
+   }
+}
---
+++ source/Acando.TimeReporter/Acando.TimeReporter.ViewModelSyncWorker/Contracts/ITimeReportViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acando.TimeReporter.ViewModelSyncWorker.Contracts
+{
+    public interface ITimeReportViewModel
+    {
+        string UserName { get; set; }
+        int WeekNumber { get; set; }
+        int Year { get; set; }
+        Dictionary<DayOfWeek, List<IReport>> Reports { get; set; }
+    }
+}
