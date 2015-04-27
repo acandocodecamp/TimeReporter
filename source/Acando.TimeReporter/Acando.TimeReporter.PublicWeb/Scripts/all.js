cb557f721a02649824adee11f8f7028793b9f182
Jesper <jesper.o.astrom@gmail.com>
2015-04-27 19:20:54 +0200

    Added Angular and bootstrap

---
+++ source/Acando.TimeReporter/Acando.TimeReporter.PublicWeb/Scripts/all.js
@@ -0,0 +1,31 @@
+﻿(function (angular) {
+
+    var app = angular.module('app', ['ngRoute']);
+    app.config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.
+          when('/MyReports', {
+              templateUrl: 'MyReports.html',
+              controller: 'MyReportsController'
+          }).
+          when('/', {
+              templateUrl: 'ReportTime.html',
+              controller: 'ReportTimeController'
+          }).
+          otherwise({
+              redirectTo: '/'
+          });
+    }]);
+
+    app.controller('ReportTimeController', ['$scope', ReportTimeController]);
+
+    function ReportTimeController() {
+
+    }
+    
+    app.controller('MyReportsController', ['$scope', MyReportsController]);
+
+    function MyReportsController() {
+
+    }
+
+})(window.angular);
